46416bc117608652ee040b559bf38e11eab66365
evless <evless56@gmail.com>
2020-01-10 11:42:30 +0300

    GINF-545454: add github token to config

--- .grenrc.js
+++ .grenrc.js
@@ -7,7 +7,7 @@ module.exports = {
     "groupBy": false,
     template: {
         issue: ({ text, url, name }) => (
-            `- ${name.replace(/(GINF-\d*)/, '[$1](https://jira.csssr.io/browse/$1)')} ([${text}](${url}))`
+            `- ${name.replace(/(GINF-\d*)/, '[$1](https://jira.csssr.io/browse/$1)')} [${text}](${url})`
         ),
         changelogTitle: '# Изменения\n\n',
     }
